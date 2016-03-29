9b00b03201efaf4bab1a3acf1378a6824c3410f5
dwpierce <dwpierce@wsu.edu>
2016-03-28 17:06:21 -0700

    Updates March 28, 2016
    Here is work done today.
    
    1. Fixed the archive page display by adding .clearfix:after css and adding overflow: hidden; to .archive selector.
    
    2. Finished work on adding the custom field using edit_form_after_title. The field will hold YouTube's video ID.
    
    3. Uploaded better thumb and picture for the accountability test module.
    
    4. Created 7 example modules to display on the home page and on the navigation menu.
    
    Updated:
    
    index.php
    category.php
    functions.php
    parts/secondary-video.php
    parts/secondary-excerpt.php
    parts/home-gallery.php

--- functions.php
+++ functions.php
@@ -52,10 +52,72 @@ function enqueue_scripts() {
  wp_enqueue_script( 'video-overlay', get_bloginfo( 'stylesheet_directory' ) . '/js/vid.js', array( 'jquery' ), '1.0.0' );
 }
 
-add_action( 'edit_form_after_title', 'myprefix_edit_form_after_title' );
-function myprefix_edit_form_after_title() {
-    echo '<h2>YouTube Video ID</h2>';
+//add video id between title and editor
+
+add_action( 'edit_form_after_title', 'wrpl_create_new_metboax_context' );
+
+function wrpl_create_new_metboax_context( $post ) {
+	
+	do_meta_boxes( null, 'custom-metabox-holder', $post );
 }
+
+
+function wrpl_add_custom_meta_box() {
+ 
+        add_meta_box(
+            'awesome_metabox_id',
+            __( 'YouTube Video Data', 'wrpldev' ),
+            'wrpl_render_metabox',
+            'page',
+            'custom-metabox-holder'    //placeholder for meta box between title and editor
+        );
+    
+}
+add_action( 'add_meta_boxes', 'wrpl_add_custom_meta_box' );
+
+//Render metabox form fields
+function wrpl_render_metabox($post) {
+
+	$videoID = get_post_meta( $post->ID,'_wrpl_meta_box_text',TRUE );
+    wp_nonce_field( 'wrpl_meta_box_nonce', 'meta_box_nonce' );
+    ?>
+    <p>
+        <div class="wrpl-meta-admin">
+	    <label for="wrpl_meta_box_text">YouTube ID</label>
+        <input type="text" name="_wrpl_meta_box_text" id="wrpl_meta_box_text" value="<?php echo $videoID; ?>" />
+        </div>
+    </p>    
+ <?php
+
+}
+
+add_action( 'save_post', 'wrpl_save_custom_meta_box' ); 
+
+// Save YouTube video ID in _wrpl_meta_box_text field
+function wrpl_save_custom_meta_box ($post_id) {
+	
+	  // Bail if we're doing an auto save
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+     
+    // if our nonce isn't there, or we can't verify it, bail
+    if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'wrpl_meta_box_nonce' ) ) return;
+     
+    // if our current user can't edit this post, bail
+    if( !current_user_can( 'edit_post' ) ) return;
+	
+	// now we can actually save the data
+    $allowed = array( 
+        'a' => array( // on allow a tags
+            'href' => array() // and those anchors can only have href attribute
+        )
+    );
+	
+	// Make sure your data is set before trying to save it
+    if( isset( $_POST['_wrpl_meta_box_text'] ) )
+        update_post_meta( $post_id, '_wrpl_meta_box_text', wp_kses( $_POST['_wrpl_meta_box_text'], $allowed ) );
+	
+}
+
 
 
 
--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 <?php get_header(); ?>
- <main> 
-   <ul>
-      <?php   
-	  
-     if ( have_posts() ) { 
-	   
-	   get_template_part( 'parts/secondary', 'nav' );
-	   
-	   
-			 while ( have_posts() ) {
-				the_post(); 
-					//	 
-	   
-		echo '<li><a href="' . get_permalink() . '" title="' . the_title_attribute() . '">';
-					the_title(); 
-				echo '</a></li>';
-					//
-				} // end while
-      } //end if have_posts
-			  
-  ?>			  
-  </ul>
+  <main>
+    <?php get_template_part( 'parts/secondary', 'nav' ); ?> 
+    <?php get_template_part( 'parts/excerpt', 'title' ); ?>  
+
+    <?php get_template_part( 'parts/secondary', 'excerpt' );?>
 </main>
 <?php get_footer(); ?>
