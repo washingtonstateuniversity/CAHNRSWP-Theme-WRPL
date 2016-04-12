60b20609df726c6ede09ccdbc6ab4279c04263db
dwpierce <dwpierce@wsu.edu>
2016-04-11 17:42:34 -0700

    Updates April 11, 2016
    On the hamburger menu and display of the under the hamburger.
    
    I updated theme files for header, footer and style.
    
    Updated files:
    
    Style.css
    header.php
    functions.php
    parts/header-nav.php
    images/menu.php

--- parts/header-nav.php
+++ parts/header-nav.php
@@ -1,14 +1,30 @@
 <div id="header-nav">
 	<div class="content-wrap">
+   <div class="hamburger"></div>  
+     
+     <ul id="outside-header-nav-menu">
+       
+
+      <li><a href="/">HOME</a></li>
+         <li><a href="/learn/">LEARN</a></li>
+       <div id="accordion">  
          <?php 
+ 
+	
 		 if ( has_nav_menu( 'header-menu')) {
 		    wp_nav_menu(array( 
 		      'theme_location' => 'header-menu',
 			  'menu' => 'header-menu',
-		      'orderby' => 'menu_order' 
+		      'orderby' => 'menu_order', 
+			  'container' => false
 			   )
 			  );
 		  }
+		  
+	
 			?>
+            <li><a href="/contact-us/">CONTACT US</a></li>   
+      </ul>
+      </div>
     </div>
 </div>
