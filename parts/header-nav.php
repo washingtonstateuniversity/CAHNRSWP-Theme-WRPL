8b20c33b55d9fdb3f67283b6bd092f501cf1656e
dwpierce <dwpierce@wsu.edu>
2016-04-12 17:38:08 -0700

    Updates April 12, 2016
    ! have the navigation for the hamburger and menus working today with your help on the javascripting.
    
    Here is a list of the updated files.
    
    Updated files:
    
    Style.css
    Functions.php
    Parts/header-nav.php
    Js/Menu.js

--- parts/header-nav.php
+++ parts/header-nav.php
@@ -1,13 +1,15 @@
 <div id="header-nav">
 	<div class="content-wrap">
    <div class="hamburger"></div>  
-     
+  <!--    <img src="wp-content/themes/CAHNRSWP-Theme-WRPL/images/menu.png" alt="menu icon" class="menu-icon" width="40" height="40"> -->
+  <!--   <div class="menu-icon"></div> -->
      <ul id="outside-header-nav-menu">
        
 
-      <li><a href="/">HOME</a></li>
-         <li><a href="/learn/">LEARN</a></li>
-       <div id="accordion">  
+      <li><a href="/wrpl/">HOME</a></li>
+         <li><a class="dropdown-toggle" href="learn/" >LEARN</a>
+   <!--    <div id="accordion">  -->
+   		
          <?php 
  
 	
@@ -16,15 +18,18 @@
 		      'theme_location' => 'header-menu',
 			  'menu' => 'header-menu',
 		      'orderby' => 'menu_order', 
-			  'container' => false
+			  'container' => 'ul',
+			  'menu_class' => 'dropdown',
 			   )
-			  );
+			 );
 		  }
 		  
 	
 			?>
-            <li><a href="/contact-us/">CONTACT US</a></li>   
+ <!--            </div> -->
+              </li>
+            <li><a href="contact-us/">CONTACT US</a></li>   
       </ul>
-      </div>
+     
     </div>
 </div>
