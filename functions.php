e79a1981ddabc7b6934e0cf4a800dec0404be8d8
dwpierce <dwpierce@wsu.edu>
2016-04-12 17:37:21 -0700

    Updates April 12, 2016
    ! have the navigation for the hamburger and menus working today with your help on the javascripting.
    
    Here is a list of the updated files.
    
    Updated files:
    
    Style.css
    Functions.php
    Parts/header-nav.php
    Js/Menu.js

--- functions.php
+++ functions.php
@@ -128,16 +128,18 @@ function my_scripts_method() {
 if ( !is_admin() ) {
 	wp_enqueue_script('jquery-ui-accordion');
 	wp_enqueue_script(
-		'custom-accordion',
-		get_stylesheet_directory_uri() . '/js/accordion.js',
+		'custom-dropdown',
+		get_stylesheet_directory_uri() . '/js/menu.js',
 		array('jquery')
 		);
+	
+	
 	}
 }
+
 add_action('wp_enqueue_scripts', 'my_scripts_method');
 
-wp_register_style('jquery-custom-style', get_stylesheet_directory_uri().'/css/jquery-ui-1.8.23.custom.css', array(), '1', 'screen'); 
-wp_enqueue_style('jquery-custom-style');
+
 
 
 ?>
