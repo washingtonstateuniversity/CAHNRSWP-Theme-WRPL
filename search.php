3a4d1cc4871f1b6dabac7f4aa0dbc6ec4708776f
dwpierce <dwpierce@wsu.edu>
2016-04-13 18:17:25 -0700

    Added files via upload
    Added some extra javascript to hide the submenu under learn when the hamburger is not showing. It's a little clunky and needs more work. I cleaned up the padding on hamburger and left nav menu in 900px mode. I also started creating the search template and search page.
    
    Here is a list of the updated files.
    
    Updated files:
    
    Style.css
    Menu.js
    Search.php
    Parts/secondary-search.php
    Parts/search-title.php

---
+++ search.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Template Name: Search Page
+*/
+?>
+ <?php
+global $query_string;
+
+$query_args = explode("&", $query_string);
+$search_query = array();
+
+if( strlen($query_string) > 0 ) {
+	foreach($query_args as $key => $string) {
+		$query_split = explode("=", $string);
+		$search_query[$query_split[0]] = urldecode($query_split[1]);
+	} // foreach
+} //if
+
+$search = new WP_Query($search_query);
+
+?>
+
+<?php
+global $wp_query;
+$total_results = $wp_query->found_posts;
+?>
+
+
+
+<?php get_header(); ?>
+
+ <div class="wrapper">
+     <div class="content-wrap">
+  
+    <?php get_template_part( 'parts/secondary', 'nav' ); ?> 
+    
+    <main>
+    
+    <?php get_template_part( 'parts/search', 'title' ); ?>  
+    <?php get_template_part( 'parts/secondary', 'search' );?>
+
+     
+    
+    </main>
+   </div> <!-- End .content-wrap -->   
+</div><!--end wrapper-->
+    
+ 
+<?php get_footer(); ?>
