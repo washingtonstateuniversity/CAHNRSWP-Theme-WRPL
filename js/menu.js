0350895b1f4f9222e26845ca0d5532aaf11c43a5
dwpierce <dwpierce@wsu.edu>
2016-04-13 18:17:51 -0700

    Updates April 13, 2016
    Added some extra javascript to hide the submenu under learn when the hamburger is not showing. It's a little clunky and needs more work. I cleaned up the padding on hamburger and left nav menu in 900px mode. I also started creating the search template and search page.
    
    Here is a list of the updated files.
    
    Updated files:
    
    Style.css
    Menu.js
    Search.php
    Parts/secondary-search.php
    Parts/search-title.php

--- js/menu.js
+++ js/menu.js
@@ -5,10 +5,16 @@ jQuery(document).ready(function () {
 	jQuery('#outside-header-nav-menu').slideToggle('fast');
 	
 	});	
+     if (document.documentElement.clientWidth < 700) {
+		     
+	
 	jQuery('#outside-header-nav-menu a.dropdown-toggle').on('click', function(event) {
 		event.preventDefault();
 		jQuery(this).siblings('ul').slideToggle('fast');
 		});
 		
+	 } else {
+		 jQuery('#menu-pd-menu').hide();
+		 }
 		
 }); 
