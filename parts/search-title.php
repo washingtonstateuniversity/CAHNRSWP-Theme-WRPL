d884a65a0e8e42bdcba083a2a15e94691050708f
dwpierce <dwpierce@wsu.edu>
2016-04-15 16:52:45 -0700

    Updates April 15, 2016
    I did some more cleaned up of the theme. I'm still working on styling the search results. Working style to add a thumbnail at this point.
    
    Here is a list of the updated files.
    
    Style.css
    Header.php
    Search.php
    Parts/secondary-search.php
    Parts/secondary-search2.php
    Parts/search-title.php

--- parts/search-title.php
+++ parts/search-title.php
@@ -3,5 +3,4 @@
         <h2><?php wp_title();  ?></h2>
           
  
-
       </section>
--- parts/secondary-search.php
+++ parts/secondary-search.php
@@ -1,15 +1,17 @@
  <section class="content-area">
        <div class="content">
+       <div class="search-results-content">
        <p></p>
         <?php get_search_form(); ?>
         <p></p>
 <!--        	<h1>Search Results</h1> -->
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <!--    <h3 class="search-title"> -->
- <div class="s-results">
+<!-- <div class="s-results"> -->
 	<a href="<?php the_permalink()?>" >
     <?php
-//	echo '<article class="clearfix">';
+
+echo '<article class="clearfix">';
     echo '<div class="search-thumb" >';
 					if ( has_post_thumbnail($search->post->ID) ) {
        				   the_post_thumbnail(array(100, 100));
@@ -18,21 +20,26 @@
 					  echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) 
         . '/images/gallery-item-banner.jpg" width="100" height="100" />';
 					}
-				echo '</div>';
-	?>			
-	<?php the_title(); ?></a>
+ 			echo '</div>';
+	 echo '<div class="search-results-text">';
+	?><h3>			
+	<?php the_title(); ?></a></h3>
 <!--	</h3> -->
-    <p><?php the_excerpt(); ?></p>
+    <?php  echo '<div class="search-results-excerpt">'; ?>
+    <?php the_excerpt(); ?><a href="<?php the_permalink()?>" >more » </a>
     </div>
+    </div> 
+
 <?php 
-// echo '</article>'; 
+echo '</article>'; 
 
 endwhile; else: ?>
        <div class="entry"><h2><?php _e('Sorry, no posts matched your Search criteria.'); ?></h2></div>
   <?php endif; ?>
-<!--	There were: <strong><?php echo $total_results; ?></strong> search results. -->
+<!--	There were: <strong><?php  ?></strong> search results. -->
 
 
-
+<?php echo paginate_links( $args ); ?>
+       </div>
        </div>
       </section>
