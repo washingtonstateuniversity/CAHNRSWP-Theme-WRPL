b73dabfdb25762bde75fecfb67d24a794d83736c
dwpierce <dwpierce@wsu.edu>
2016-04-14 17:43:36 -0700

    Updates April 14, 2016
    I did some more cleaned up of the theme. I'm still working on styling the search results. Working style to add a thumbnail at this point.
    
    Here is a list of the updated files.
    
    Updated files:
    
    Style.css
    Header.php
    Search.php
    Parts/secondary-search.php
    Parts/secondary-search2.php
    Parts/search-title.php

--- parts/secondary-search.php
+++ parts/secondary-search.php
@@ -3,12 +3,31 @@
        <p></p>
         <?php get_search_form(); ?>
         <p></p>
-       	<h1>Search Results</h1>
+<!--        	<h1>Search Results</h1> -->
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<h2 class="post-title">
-	<a href="<?php the_permalink()?>" ><?php the_title(); ?></a>
-	</h2>
-<?php endwhile; else: ?>
+<!--    <h3 class="search-title"> -->
+ <div class="s-results">
+	<a href="<?php the_permalink()?>" >
+    <?php
+//	echo '<article class="clearfix">';
+    echo '<div class="search-thumb" >';
+					if ( has_post_thumbnail($search->post->ID) ) {
+       				   the_post_thumbnail(array(100, 100));
+					} 
+					else {
+					  echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) 
+        . '/images/gallery-item-banner.jpg" width="100" height="100" />';
+					}
+				echo '</div>';
+	?>			
+	<?php the_title(); ?></a>
+<!--	</h3> -->
+    <p><?php the_excerpt(); ?></p>
+    </div>
+<?php 
+// echo '</article>'; 
+
+endwhile; else: ?>
        <div class="entry"><h2><?php _e('Sorry, no posts matched your Search criteria.'); ?></h2></div>
   <?php endif; ?>
 <!--	There were: <strong><?php echo $total_results; ?></strong> search results. -->
---
+++ parts/secondary-search2.php
@@ -0,0 +1,20 @@
+ <section class="content-area">
+       <div class="content">
+       <p></p>
+        <?php get_search_form(); ?>
+        <p></p>
+<!--        	<h1>Search Results</h1> -->
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<h2 class="post-title">
+	<a href="<?php the_permalink()?>" ><?php the_title(); ?></a>
+	</h2>
+    <p><?php the_excerpt(); ?></p>
+<?php endwhile; else: ?>
+       <div class="entry"><h2><?php _e('Sorry, no posts matched your Search criteria.'); ?></h2></div>
+  <?php endif; ?>
+<!--	There were: <strong><?php echo $total_results; ?></strong> search results. -->
+
+
+
+       </div>
+      </section>
