3fd3cab4818ede327a09875f6bc3bb68b4c6053c
dwpierce <dwpierce@wsu.edu>
2016-04-18 17:30:56 -0700

    Updates April 18, 2016
    I finished the search results display page and made updated the archive/category display page to look similar. I ended up using  a substr function to cut down the number of words displayed since 'excerpt_length' only works on posts. I also tested the Page Builder 2.0 on the development site. I found the YouTube URL field was moved down below the Page Builder editor.
    
    Here is a list of the updated files.
    
    Updated files:
    
    Style.css
    Functions.php
    Parts/secondary-search.php
    Parts/secondary-excerpt.php

--- functions.php
+++ functions.php
@@ -139,17 +139,6 @@ if ( !is_admin() ) {
 
 add_action('wp_enqueue_scripts', 'my_scripts_method');
 
-/**
- * Filter the except length to 20 characters.
- *
- * @param int $length Excerpt length.
- * @return int (Maybe) modified excerpt length.
- */
-function wpdocs_custom_excerpt_length( $length ) {
-    return 20;
-}
-add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
-
 
 
 ?>
