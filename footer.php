18651573c038e0a164e89e3aa5633950a5a55a6c
dwpierce <dwpierce@wsu.edu>
2016-06-28 08:24:11 -0700

    Added contact information for live site to footer

--- footer.php
+++ footer.php
@@ -4,9 +4,10 @@
  <div class="content-wrap">
  <p></p>
   <p>Western Regional Program Leaders<br />
-    Address<br />
-    City, State Zip<br />
-    Phone</p>
+    Washington State University<br />
+    P.O.Box 1495<br />
+    Spokane, WA 99210-1495<br />
+    509.358.7686</p>
  </div>
 </div>
 </footer>
